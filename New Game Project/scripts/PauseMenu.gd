4dc7d0c5933412f654881626e6f6a8eb15e748c5
Topi Nieminen <budy426@gmail.com>
2019-02-13 02:08:44 +0200

    Started working on a pause menu.

---
+++ New Game Project/scripts/PauseMenu.gd
@@ -0,0 +1,41 @@
+extends CanvasLayer
+
+# Number of buttons in the pause menu.
+const NUM_OF_BTN = 2
+# Keeps track of focused button.
+var index = 0
+
+func _ready():
+	$Panel.hide()
+	$Panel/ContinueBtn.connect("pressed", self, "_on_ContinueBtn_pressed")
+	$Panel/QuitBtn.connect("pressed", self, "_on_QuitBtn_pressed")
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_escape"):
+		pass
+	if Input.is_action_just_pressed("ui_up"):
+		changeFocus(-1)
+	if Input.is_action_just_pressed("ui_down"):
+		changeFocus(1)
+
+func changeFocus(direction):
+	index = clamp(index+direction, 0, NUM_OF_BTN-1)
+	
+	if index == 0:
+		$Panel/ContinueBtn.grab_focus()
+	elif index == 1:
+		$Panel/QuitBtn.grab_focus()
+	
+func toggleMenu():
+	if visible():
+		$Panel.hide()
+		$Panel.release_focus()
+	else:
+		Panel.show()
+		$Panel/ContinueBtn.get_focus()
+		
+func _on_ContinueBtn_pressed():
+	toggleMenu()
+
+func _on_QuitBtn_pressed():
+	get_tree().quit()
