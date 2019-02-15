ea343cf0fad8bf6befd02dffb1b7ac5cc9774ee8
Topi Nieminen <nieminen.topi.m@student.uta.fi>
2019-02-15 15:43:14 +0200

    Rage merged changes from repo Unilifegame.

---
+++ New Game Project/scenes/nextworld.gd
@@ -0,0 +1,9 @@
+#nextworld.gd
+extends Area2D
+
+func _physics_process(delta):
+	var bodies = get_overlapping_bodies()
+	print(bodies)
+	for body in bodies:
+		if body.name == "Player":
+			get_tree().change_scene("scenes/world2.tscn")
